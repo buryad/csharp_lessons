d768da2ce95a77d244a2a84950b2d26602b4f4a3
B V <bv@gm.com>
2023-08-06 23:13:06 +0700

    Update

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,5 +1,4 @@
-﻿int numberA = new Random().Next(1, 10);
-Console.WriteLine(numberA);
-int numberB = new Random().Next(1, 10);
-Console.WriteLine(numberB);
-Console.WriteLine(numberA + numberB);
+﻿int numberA = 3;
+int numberB = 5;
+int result = numberA + numberB;
+Console.WriteLine(result);
